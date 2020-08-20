63131b82bea3bd6826fd6d8c20524a1d55ddd41a
Austin Aspaas <araspaas@live.com>
2020-08-20 17:32:57 -0600

    Add shelters to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Shelter.create(name: "Van's pet shop", address: "3724 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
+Shelter.create(name: "Bovice's pet shop", address: "37 ed vera dr", city: "Rockford", state: "Illinois", zip: "61109")
+Shelter.create(name: "Jared's pet shop", address: "24 simpson rd", city: "Rockford", state: "Illinois", zip: "61102")
+Shelter.create(name: "Old Greg's pet shop", address: "24 rona st", city: "Rockford", state: "Illinois", zip: "61102")
