d0a07c39ce225cbea4a89b821df766df1daed6a9
Austin Aspaas <araspaas@live.com>
2020-08-20 18:07:16 -0600

    Moved my before :each to the correct position

--- spec/features/visitor/visitor_visits_shelter_index_spec.rb
+++ spec/features/visitor/visitor_visits_shelter_index_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "visitors" do
+  before :each do
+    @shelter1 = Shelter.create(name: "Van's pet shop", address: "3724 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
+    @shelter2 = Shelter.create(name: "Bovice's pet shop", address: "37 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
+    @shelter3 = Shelter.create(name: "Jared's pet shop", address: "24 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
+    @shelter4 = Shelter.create(name: "Old Greg's pet shop", address: "24 rona st", city: "Rockford", state: "Illinois", zip: "61102")
+  end
   describe "visit shelter index" do
-    before :each do
-      @shelter1 = Shelter.create(name: "Van's pet shop", address: "3724 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
-      @shelter2 = Shelter.create(name: "Bovice's pet shop", address: "37 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
-      @shelter3 = Shelter.create(name: "Jared's pet shop", address: "24 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
-      @shelter4 = Shelter.create(name: "Old Greg's pet shop", address: "24 rona st", city: "Rockford", state: "Illinois", zip: "61102")
-    end
 
     it "can see all shelters from shelter index" do
 
