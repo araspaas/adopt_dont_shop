29b65281a9d6179bbc0e683f3a5d75d40392ee01
Austin Aspaas <araspaas@live.com>
2020-08-22 14:01:25 -0600

    Add Create action

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,4 +7,12 @@ class SheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    Shelter.create(name: params["name"], address: params["address"], city: params["city"], state: params["state"], zip: params["zip"])
+    redirect_to '/shelters'
+  end
 end
