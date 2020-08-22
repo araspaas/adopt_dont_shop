add1e3e23382d87cce3c164b09ac70460e9e84eb
Austin Aspaas <araspaas@live.com>
2020-08-22 13:59:17 -0600

    Add post route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
+  post '/shelters', to: 'shelters#create'
+
+  # resources :shelters, only: [:create, :new, :show, :index]
 end
