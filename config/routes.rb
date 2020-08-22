74a5fb4ff6e81c2202f8c19ba05c251d704914da
Austin Aspaas <araspaas@live.com>
2020-08-22 15:55:22 -0600

    Add routes for update and edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
   post '/shelters', to: 'shelters#create'
+  patch '/shelters/:id', to: 'shelters#update'
 
   # resources :shelters, only: [:create, :new, :show, :index]
 end
