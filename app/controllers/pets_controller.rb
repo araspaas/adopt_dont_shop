f4811beae918c55966a50fe32fc803e7c5ed38c5
Austin Aspaas <araspaas@live.com>
2020-08-23 15:01:48 -0600

    Add status to Pet.create. Add @pet.save. Update path to use instance variable

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,6 @@ class PetsController < ApplicationController
     @pet = Pet.create(image: params[:image], name: params[:name], description: params[:description], age: params[:age], sex: params[:sex], shelter_id: params[:shelter_id], status: 0)
     @pet.save
     binding.pry
-    redirect_to "/shelters/#{@shelter.id}/pets"
+    redirect_to "/shelters/#{@shelter_id}/pets"
   end
 end
