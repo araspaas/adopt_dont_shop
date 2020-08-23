ca2fc5bb800612fda183e01b551b56d1ab6e5b3a
Austin Aspaas <araspaas@live.com>
2020-08-23 14:01:07 -0600

    Add a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_031536) do
+ActiveRecord::Schema.define(version: 2020_08_23_185253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_031536) do
     t.string "sex"
     t.string "shelter_id"
     t.string "description"
-    t.string "status"
+    t.integer "status"
   end
 
   create_table "shelters", force: :cascade do |t|
