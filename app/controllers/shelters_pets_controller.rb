ab348e3afaad2968c76cddbc1e5b21329b01551f
Austin Aspaas <araspaas@live.com>
2020-08-22 21:04:30 -0600

    Add sheters_pets_controller and index action

---
+++ app/controllers/shelters_pets_controller.rb
@@ -0,0 +1,8 @@
+class SheltersPetsController < ApplicationController
+
+  def index
+    @shelter = Shelter.find(params[:id])
+    all_pets = Pet.all
+    @pets = all_pets.find_all{|pet| pet.shelter_id.to_i == params[:id].to_i}
+  end
+end
